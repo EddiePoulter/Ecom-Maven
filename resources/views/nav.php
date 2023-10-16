4a836e62fc929bc408902dab16fee2c6d1dd439e
Sunaez <kurdistan201324@gmail.com>
2023-10-16 11:56:39 +0100

    index page work

---
+++ resources/views/nav.php
@@ -0,0 +1,56 @@
+<nav>
+    <div class="toprow">
+        <div class="logo">
+            <img src="/resources/images/public/Icon.png" style="width: 50px; height: 50px;">
+        </div>
+        <div class="right-side">
+            <!-- ALAND WILL DO THIS !! -->
+            <a href="index.html" class="item home">
+                <img src="/resources/images/public/nav/home.png">
+                <div class="text">
+                    Home
+                </div>
+            </a>
+            <a href="products.html" class="item products">
+            <img src="/resources/images/public/nav/product.png">
+
+                <div class="text">
+                    Products
+                </div>
+            </a>
+            <a href="about.html" class="item about">
+            <img src="/resources/images/public/nav/about.png">
+                <div class="text">
+                    About Us
+                </div>
+            </a>
+            <a href="contact.html" class="item contact">
+            <img src="/resources/images/public/nav/contact.png">
+                <div class="text">
+                    Contact
+                </div>
+            </a>
+            <a href="account.html" class="item account">
+            <img src="/resources/images/public/nav/account.png">
+
+                <div class="text">
+                    Account
+                </div>
+            </a>
+            <a href="inventory.html" class="item inv">
+            <img src="/resources/images/public/nav/inventory.png">
+
+                <div class="text">
+                    Inventory
+                </div>
+            </a>
+            <a href="basket.html" class="item basket">
+            <img src="/resources/images/public/nav/basket.png">
+
+                <div class="text">
+                    Basket
+                </div>
+            </a>
+        </div>
+    </div>
+</nav>
