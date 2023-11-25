fa2ec1d368382e07944cfd18715a16b0cff393be
omer2k4 <remo4k2@gmail.com>
2023-11-25 19:54:38 +0000

    Products page
    I added price, images and buttons to the product page. I also worked on the CSS code for it.

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -14,16 +14,180 @@
 
     <?php include "footer.blade.php"; ?>
 
-<div class="small-container">
-    <h2>Products</h2>
-    <div class="row">
-        <div class="col-4">
-            <img src="C:\xampp\htdocs\Maven\resources\images\public\product images\skiingcoat.png">
-            <h4>Blue Ski Jacket</h4>
-            <p>£49.99</p>
-        </div>
+    <title>Products</title>
+
+<!-- Bootstrap core CSS -->
+<link href="../../../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="products.css" rel="stylesheet">
+</head>
+
+<body>
+
+<header>
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1 class="jumbotron-heading">Products</h1>
     </div>
-</div>
+  </section>
+
+  <div class="products py-5 bg-light">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top" img src="C:\xampp\htdocs\Maven\resources\images\public\product images\skiingequipmentset.png">
+            <div class="card-body">
+              <p class="card-text">Unisex Ski(178cm) Equipment Set</p>
+              <p>£284.99<p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top" img src="C:\xampp\htdocs\Maven\resources\images\public\product images\skiingcoat.png">
+            <div class="card-body">
+              <p class="card-text">Blue Ski Jacket</p>
+              <p>£49.99<p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                </div>   
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top" img src="C:\xampp\htdocs\Maven\resources\images\public\product images\pairofski.jpg">
+            <div class="card-body">
+              <p class="card-text">Unisex Ski (178cm)</p>
+              <p>£94.99<p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top" data-src="C:\xampp\htdocs\Maven\resources\images\public\product images\skigoggles2.jpg" >
+            <div class="card-body">
+              <p class="card-text">Unisex Ski Goggles</p>
+              <p>£59.99<p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                </div> 
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top" img src="C:\xampp\htdocs\Maven\resources\images\public\product images\skihelmet.jpg">
+            <div class="card-body">
+              <p class="card-text">Unisex Ski Helmet</p>
+              <p>£79.99<p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                </div>  
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top" img src="C:\xampp\htdocs\Maven\resources\images\public\product images\snowboard.jpg" >
+            <div class="card-body">
+              <p class="card-text">Unisex Snowboard(155cm)</p>
+              <p>£309.99<p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top" img src="resources/images/public/product images/pairofskiandpoleset.jpg" >
+            <div class="card-body">
+              <p class="card-text">Unisex Ski(178cm) and Pole Set</p>
+              <p>£169.99<p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top" img src="C:\xampp\htdocs\Maven\resources\images\public\product images\skipoles.jpg" >
+            <div class="card-body">
+              <p class="card-text">Unisex Ski Poles</p>
+              <p>£69.99<p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" >
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</main>
+
+<footer class="text-muted">
+  <div class="container">
+    <p class="float-right">
+      <a href="#">Back to top</a>
+    </p>
+    <p>products example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+    <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+  </div>
+</footer>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+<script src="../../../../assets/js/vendor/popper.min.js"></script>
+<script src="../../../../dist/js/bootstrap.min.js"></script>
+<script src="../../../../assets/js/vendor/holder.min.js"></script>
+</body>
+</html>
+
 
 
 
