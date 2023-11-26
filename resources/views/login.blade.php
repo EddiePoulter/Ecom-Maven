44d52d1cf8b91691cd2c4aab99dafdf2b6de9f42
Talhah-1 <talhah.altafi@hotmail.com>
2023-11-26 19:27:32 +0000

    fixed login and signup pages
    fixed

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,14 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+    
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>log in to Maven</title>
+    <title>Login to Maven</title>
+    <link rel="icon" href="/resources/images/public/Icon.png">
+    <link rel="stylesheet" href="/Ecom-Maven/resources/css/login.css">
     <?php include "css.blade.php"; ?>
-</head>
-<body>
-    <?php include "nav.blade.php"; ?>
 
+</head>
+
+<nav>
+  <?php include("nav.blade.php"); ?>
+</nav>
+    
+    <div class="login-container">
+        <div class="formation">
+        <h1>Login to Maven</h1>
+        <form action="index.php" method="post">
+            <input type="email" name="email" placeholder="Enter your email address" required>
+            <input type="password" name="password" placeholder="Enter your password" required>
+            <input type="submit" value="Login" />
+            <input type="button" value="Register" onclick="window.location.href='signup.blade.php'" />
+            <?php if (!empty($error_message)) { ?>
+                <p class="error-message"><?php echo $error_message; ?></p>
+            <?php } ?>
+            <input type="hidden" name="submitted" value="TRUE" />
+            <p><a href="forgot_password.php"><br>Forgot Your Password?</a></p>
+        </form>
+        </div>
+    </div>
+
+<footer>
     <?php include "footer.blade.php"; ?>
-</body>
-</html>
+</footer>
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -3,12 +3,70 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>sign up to Maven</title>
+    <title>Sign up to Maven</title>
+    <link rel="stylesheet" href="/Ecom-Maven/resources/css/signup.css">
     <?php include "css.blade.php"; ?>
 </head>
-<body>
-    <?php include "nav.blade.php"; ?>
 
+
+<div class="register-container">
+<div class="formation">
+  <h2>Register a Maven Account</h2><br>
+  <form method="post" action="register.php">
+    <input type="email" name="email" id="email" placeholder="Enter your email address" required /><br>
+    <input type="email" name="confirm_email" id="confirm_email" placeholder="Confirm your email address" required />
+    <span id="email_match"></span><br>
+    <input type="password" name="password" id="password" placeholder="Enter your password" required /><br>
+    <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm your password" required />
+    <span id="password_match"></span><br>
+    <input type="tel" name="phone" id="phone" placeholder="Enter your phone number" required />
+    <span id="phone_match"></span><br>
+    <input type="submit" value="Register" />
+    <input type="reset" value="Clear" />
+    <input type="hidden" name="submitted" value="true" />
+    <p> <br> Already a user? <a href="login.blade.php">Log in</a></p>
+  </form>
+</div>
+</div>
+
+</div>
+  <script>
+    document.getElementById('confirm_email').addEventListener('input', function () {
+      var email = document.getElementById('email').value;
+      var confirm_email = document.getElementById('confirm_email').value;
+      if (email === confirm_email) {
+        document.getElementById('email_match').style.color = 'green';
+        document.getElementById('email_match').innerHTML = '';
+      } else {
+        document.getElementById('email_match').style.color = 'red';
+        document.getElementById('email_match').innerHTML = 'Emails do not match.';
+      }
+    });
+    document.getElementById('confirm_password').addEventListener('input', function () {
+      var password = document.getElementById('password').value;
+      var confirm_password = document.getElementById('confirm_password').value;
+      if (password === confirm_password) {
+        document.getElementById('password_match').style.color = 'green';
+        document.getElementById('password_match').innerHTML = '';
+      } else {
+        document.getElementById('password_match').style.color = 'red';
+        document.getElementById('password_match').innerHTML = 'Passwords do not match.';
+      }
+    });
+    document.getElementById('phone').addEventListener('input', function () {
+    var phone = document.getElementById('phone').value;
+    if (phone.length === 10) {
+      document.getElementById('phone_match').style.color = 'green';
+      document.getElementById('phone_match').innerHTML = '';
+    } else {
+      document.getElementById('phone_match').style.color = 'red';
+      document.getElementById('phone_match').innerHTML = 'Enter a valid phone number';
+    }
+  });
+  </script>
+<footer>
+<body>
     <?php include "footer.blade.php"; ?>
 </body>
+</footer>
 </html>
