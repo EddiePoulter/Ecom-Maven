15ff90f2e4877c0fba61508ab350af39c022279c
tekika1 <126341856+tekika1@users.noreply.github.com>
2024-03-13 12:25:12 +0000

    Create HomeController.php

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    //
+    public function index(){
+        return view('home.userpage');
+    }
+    public function redirect(){
+        $usertype=Auth::user()->usertype;
+
+        if($usertype== "1"){
+            $total_product=product::all()-> count();
+
+            
+
+            $total_user=user::all()-> count();
+            return view('admin.home',compact('total_product','total_user'));
+        
+    }
+
+    else{
+        return view('home.userpage');
+    }
+}
+
+}
