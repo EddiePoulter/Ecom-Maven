2f2e4e1c8fc2fab2212e09cae7d5f2c6924a7213
tekika1 <126341856+tekika1@users.noreply.github.com>
2024-03-14 15:11:51 +0000

    Update 2023_11_22_190931_create_products_table.php

--- database/migrations/2023_11_22_190931_create_products_table.php
+++ database/migrations/2023_11_22_190931_create_products_table.php
@@ -16,8 +16,9 @@ return new class extends Migration
             $table->string("name")->nullable();
             $table->longtext("description")->nullable();
             $table->integer("price");
-            $table->string('quantity')->nullable;
-            $table->string('discount_price')->nullable;
+            $table->string('category')->nullable();
+            $table->string('quantity')->nullable();
+            $table->string('discount_price')->nullable();
             $table->string("image_path")->nullable(); // New column for storing image path
             $table->timestamps();
         });
